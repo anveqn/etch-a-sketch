15e66cc6aa36831c28120cc843c8d582b2d9f38a
anveqn <205069225+anveqn@users.noreply.github.com>
2025-05-03 19:31:38 +0200

    Add a way to reset the board and choose desired size

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const mainBox = document.querySelector("#container");
-let chosenSize = 16;
+const sizeButton = document.querySelector("#size");
 
-for (let i = 0; i<chosenSize*chosenSize; i++){
-  let innerBox = document.createElement("div");
-  innerBox.setAttribute("class", "pixel");
-  innerBox.setAttribute("style", `width: ${700/chosenSize}px; height: ${700/chosenSize}px;`)
-  innerBox.addEventListener("mouseover", function(){innerBox.setAttribute("class", "active");})
-  mainBox.appendChild(innerBox);
+let boardSetup = () => {
+  let chosenSize = prompt("Choose the number of squares per side (max 100)");
+  for (let i = 0; i<chosenSize*chosenSize; i++){
+    let innerBox = document.createElement("div");
+    innerBox.setAttribute("class", "pixel");
+    innerBox.setAttribute("style", `width: ${700/chosenSize}px; height: ${700/chosenSize}px;`)
+    innerBox.addEventListener("mouseover", function(){innerBox.setAttribute("class", "active");})
+    mainBox.appendChild(innerBox);
+  };
 };
+
+let deleteBoard = () => {
+  while (mainBox.firstChild){
+    mainBox.removeChild(mainBox.firstChild);
+  };
+};
+
+sizeButton.addEventListener("click", function(){
+  deleteBoard();
+  boardSetup();
+});
