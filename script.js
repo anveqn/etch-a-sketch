4c577648a2809f81cca78519a93fb62d4483a7d0
anveqn <205069225+anveqn@users.noreply.github.com>
2025-05-03 19:13:06 +0200

    Add coloring on hover

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const mainBox = document.querySelector("#container");
-let chosenSize = 50;
+let chosenSize = 16;
 
 for (let i = 0; i<chosenSize*chosenSize; i++){
   let innerBox = document.createElement("div");
-  innerBox.textContent = ".";
   innerBox.setAttribute("class", "pixel");
   innerBox.setAttribute("style", `width: ${700/chosenSize}px; height: ${700/chosenSize}px;`)
+  innerBox.addEventListener("mouseover", function(){innerBox.setAttribute("class", "active");})
   mainBox.appendChild(innerBox);
 };
